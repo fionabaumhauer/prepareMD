728b3443facc25e960ebc2bfdb8d1b8910398e46
Fiona Baumhauer <fb590@wildcat>
2026-01-16 11:53:18 +0000

    calculating electrostatic exponential force from parameters

---
+++ prototype/fixingforces/prepareMD.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# activate conda environment
+source /home/fb590/miniconda3/etc/profile.d/conda.sh
+conda activate prepareMD
+
+# create packmol.inp file
+python /scratch/fb590/co2n2/electric/prepareMD/prototype/scripts/write_packmol_inp.py
+
+# get sim_id from directory name
+dir_name=$(basename "$PWD")
+sim_id=${dir_name#sim_}
+
+# get average numbers of molecules
+averagesfile="/scratch/fb590/co2n2/electric/prepareMD/prototype/gcmc_averages.txt"
+read CO2 N2 < <(awk -v sim="$sim_id" 'BEGIN{FS="\t"} $1==sim {printf "%.0f %.0f", $3, $4}' "$averagesfile")
+
+# multiply by 9 because MD box is 90x90x30 rather than 30x30x30                                                                                                                                                                                                                            
+CO2_forMD=$(( CO2 * 1 ))
+N2_forMD=$(( N2 * 1 ))
+
+# edit packmol input file to include the number of molecules and run to output temp.xyz
+sed -i "s/NUMBER_N2/${N2_forMD}/g" packmol.inp
+sed -i "s/NUMBER_CO2/${CO2_forMD}/g" packmol.inp
+packmol -i packmol.inp
+ 
+# take coordinates from temp.xyz and put them in a data file
+python /scratch/fb590/co2n2/electric/prepareMD/prototype/scripts/convert_xyz_to_data.py
+
+# make a copy of input.yaml where all LJ walls (if there are any) are zeroed by setting all epsilons to 0.0
+sed \
+    -e 's/^\(\s*epsilon_lo:\s*\).*/\10.0/' \
+    -e 's/^\(\s*epsilon_hi:\s*\).*/\10.0/' \
+    input.yaml > input_nowalls.yaml
+
+# create txt files for each species containing binned forces calculated from the external potential in input_nowalls.yaml
+python /scratch/fb590/co2n2/electric/prepareMD/prototype/scripts/make_forces_files.py
+
+# create in.lammps using parameters extracted from input.yaml
+python /scratch/fb590/co2n2/electric/prepareMD/prototype/scripts/write_lammps_input.py
--- prototype/scripts/make_forces_files.py
+++ prototype/scripts/make_forces_files.py
@@ -1,40 +1,21 @@
 """
 This script gets the external potential from input_nowalls.yaml and creates a txt file with the binned forces for each species.
 """
-
+from matplotlib import pyplot as plt
 import numpy as np
-import sys
 import yaml
 import scipy.constants as const
 
-sys.path.insert(0, '/scratch/fb590/code/gcmc-dev/src')
-from external_potentials import initialize_external_potentials
-
-def initialize_potentials(config, particle_type):
-    """
-    Function to initialize the external potentials based on the configuration.
-    
-    Parameters:
-    - config (dict): Configuration dictionary.
-    
-    Returns:
-    - beta (float): Inverse temperature (1/kB*T).
-    - Vext (function): External potential function for the given particle type.
-    - mu (float): Chemical potential for the given particle type.
-    """
-    external_potentials = initialize_external_potentials(config)
-    beta = 1 / (config['kB'] * config['T'])
-
-    Vext = external_potentials[particle_type]
-    mu = config['particle_types'][particle_type]['mu'] * config['kB'] * config['T']
-
-    return beta, Vext, mu, config['T']
-
-
 yamlfile = "input_nowalls.yaml"
-
 with open(yamlfile, 'r') as file:
     config = yaml.safe_load(file)
+
+low = config['particle_types']['C']['low']
+high = config['particle_types']['C']['high']
+phi = config['particle_types']['C']['phi_0_hi']
+d = config['particle_types']['C']['d_hi']
+beta = 1 / (config['kB'] * config['T'])
+T = config['T']
 
 bin_edges = x_positions = np.linspace(0, config['box_length_x'], 2001)
 bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
@@ -42,14 +23,19 @@ bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
 species_list = ["C", "O", "N", "X"]
 
 for species in species_list:
-    beta, Vext, mu, T = initialize_potentials(config, species)
+    mu = config['particle_types'][species]['mu'] * config['kB'] * config['T']
     xbins = np.array([bin_centers])
-    Vext_values = Vext(xbins) # Vext is a function
+    r_low = xbins[0] - low
+    r_high = high - xbins[0]
+    Vext_values_low = phi * const.Boltzmann * T * np.exp(-r_low/d) * config['particle_types'][species]['q']
+    Vext_values_high = phi * const.Boltzmann * T * np.exp(-r_high/d) * config['particle_types'][species]['q']
+    Vext_values = Vext_values_low + Vext_values_high
     muloc_profile = (Vext_values - mu) * beta
     muloc_profile_in_kcal_per_mol = muloc_profile * const.Boltzmann * T * const.Avogadro / 4184 # keeping all the beta stuff here beause it helps me visualise conversions
-
     a = np.append(bin_centers[::]-bin_centers[::][0], bin_centers[::][-1]-bin_centers[::][0]+bin_centers[::][0])
+    plt.plot(muloc_profile_in_kcal_per_mol)
     force_profile = np.gradient(-muloc_profile_in_kcal_per_mol, bin_centers) # computes d/dx (-u(x)) for each bin
-
+    plt.plot(force_profile)
+    plt.show()
     np.savetxt(f"zbins.txt", a)
     np.savetxt(f"force{species}.txt", force_profile)
